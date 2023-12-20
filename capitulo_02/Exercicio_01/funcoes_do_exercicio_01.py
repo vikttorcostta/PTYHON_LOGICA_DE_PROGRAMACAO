b0b43d72894741e73a7ee9b60782002f89c05a14
vikttorcostta <victorfotografia12@gmail.com>
2023-12-19 22:38:18 -0300

    commit

---
+++ capitulo_02/Exercicio_01/funcoes_do_exercicio_01.py
@@ -0,0 +1,59 @@
+# LIVRO: ALGORÍTOMOS - LÓGICA PARA DESENVOLVIMENTO DE COMPUTADORES 29 EDIÇÃO
+# PAULO VICTOR COSTA DE MELO
+# 19 DEZEMBRO 2023
+
+# ALUNO DOS CURSOS DE SISTEMAS DE INFORMAÇÃO, TECNÓLOGO EM BANCO DE DADOS, TÉCNICO EM DESENVOLVIMENTO DE SITEMAS
+# EXERCICIO COM PRATICAS AUT0DIDATA
+
+# 01 - DADA AS EQUAÇÕES MATEMATICAS A SEGUIR, ESTABELEÇA, PARA CADA UMA DELAS A RESPECTIVA EXPREÇÃO ARITIMÉTICA
+# ARQUIVO DE FUNÇÕES
+
+# LETRA A
+def letra_a(c, r):
+    f = (((1 * c) / 1) - ((4 * c) ** 2) / (r ** 2)) ** 2
+    return f
+
+
+# LETRA B
+def letra_b(b, c, a):
+    x = (b-c) / a
+    return x
+
+
+# LETRA C
+def letra_c(m, a, h, v):
+    x = m * ((a * h) + (v ** 2) / 2)
+    return x
+
+
+# LETRA D
+def letra_d(v, t, a):
+    d = (v * t) + ((a * t) ** 2) / 2
+    return d
+
+
+# LETRA E
+def letra_e(p, r, n):
+    d = (p - r) / n
+    return d
+
+
+# LETRA F
+def letra_f(c):
+    f = (c * (9 / 5)) + 32
+    return f
+
+
+# LETRA E
+def letra_e(f):
+    c = (f - 32) * (5 / 9)
+    return c
+
+
+
+
+
+
+
+
+
---
+++ capitulo_02/Exercicio_01/index_exercicio_01.py
@@ -0,0 +1,28 @@
+# LIVRO: ALGORÍTOMOS - LÓGICA PARA DESENVOLVIMENTO DE COMPUTADORES 29 EDIÇÃO
+# PAULO VICTOR COSTA DE MELO
+# 19 DEZEMBRO 2023
+
+# ALUNO DOS CURSOS DE SISTEMAS DE INFORMAÇÃO, TECNÓLOGO EM BANCO DE DADOS, TÉCNICO EM DESENVOLVIMENTO DE SITEMAS
+# EXERCICIO COM PRATICAS AUT0DIDATA
+
+
+# 01 - DADA AS EQUAÇÕES MATEMATICAS A SEGUIR, ESTABELEÇA, PARA CADA UMA DELAS A RESPECTIVA EXPREÇÃO ARITIMÉTICA
+# ARQUIVO PRINCIPAL
+
+import funcoes_do_exercicio_01
+
+    # valor_01 = float(input('Digite o primeiro valor'))
+    # valor_02 = float(input('Digite o segundo valor'))
+    #
+    # resultado = funcoes_do_exercicio_01.letra_a(valor_01, valor_02)
+    #
+    # print(resultado)
+
+while True:
+
+
+
+
+
+
+
